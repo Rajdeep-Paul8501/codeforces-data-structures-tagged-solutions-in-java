315ad817b2c684150e131da3ade224861bab2fc7
Rajdeep-Paul8501 <68452736+Rajdeep-Paul8501@users.noreply.github.com>
2020-07-26 15:05:06 +0530

    Create 1385C.java

---
+++ 1385C.java
@@ -0,0 +1,90 @@
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+import java.util.*; 
+import java.util.StringTokenizer;
+import java.io.DataInputStream; 
+import java.io.FileInputStream;
+import java.io.File;
+
+
+
+  
+public class cf1200A
+{ 
+    static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    }  
+  public static void main(String[] args){
+   FastReader f=new FastReader();
+   int t =f.nextInt();
+   while(t-->0){
+    int n =f.nextInt();
+    int []arr=new int[n];
+    for (int i=0;i<n;i++){
+        arr[i]=f.nextInt();
+    }
+    int pos=n-1;
+    while(pos>0&&arr[pos-1]>=arr[pos]){
+        pos--;
+    }
+    while(pos>0&&arr[pos-1]<=arr[pos]){
+        pos--;
+    }
+    System.out.println(pos);
+}
+}
+}
