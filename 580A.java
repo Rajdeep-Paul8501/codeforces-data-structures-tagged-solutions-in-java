c334aa415dd7dad4e8aa37f8a58c556dc879c6cc
Rajdeep-Paul8501 <68452736+Rajdeep-Paul8501@users.noreply.github.com>
2020-07-26 14:11:15 +0530

    Create 580A.java

---
+++ 580A.java
@@ -0,0 +1,91 @@
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+import java.util.*; 
+import java.util.StringTokenizer;
+import java.io.DataInputStream; 
+import java.io.FileInputStream;
+import java.io.File;
+
+
+
+  
+public class cf1200A
+{ 
+    static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    }  
+  public static void main(String[] args)throws Exception{
+   FastReader f=new FastReader();
+   int n =f.nextInt();
+   int []arr=new int[n];
+   for (int i=0;i<n;i++) arr[i]=f.nextInt();int count=1;int max=0;
+   for(int i=1;i<n;i++){
+    if(arr[i]>=arr[i-1]){
+        count++;
+    }
+    else{
+        if(count>max){
+            max=count;
+        }
+        count=1;
+
+    }
+
+   }
+   System.out.println(Math.max(count,max));
+}
+}
