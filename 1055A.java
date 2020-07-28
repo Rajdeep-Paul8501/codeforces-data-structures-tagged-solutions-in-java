12817a080c298387b51b65510e9a20668d589df2
Rajdeep-Paul8501 <68452736+Rajdeep-Paul8501@users.noreply.github.com>
2020-07-28 18:38:55 +0530

    Create 1055A.java

---
+++ 1055A.java
@@ -0,0 +1,168 @@
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+import java.util.*; 
+import java.util.StringTokenizer;
+import java.io.DataInputStream; 
+import java.io.FileInputStream;
+import java.io.File;
+import java.lang.*;
+import java.net.*;
+
+
+
+  
+public class Main
+{ 
+    static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    }
+    static class urlReader{
+        URL url;
+        BufferedReader br; 
+        StringTokenizer st; 
+
+        public urlReader(URL url1){
+            url = url1;
+            try{
+            br = new BufferedReader(new InputStreamReader(url.openStream()));
+        }
+        catch (IOException  e){
+            System.out.println("GIVEN URL INVALID: " + url);
+        }
+
+        }
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+
+
+    }
+    
+
+  public static void main(String[] args)throws Exception{
+    FastReader f=new FastReader();
+    int n=f.nextInt();int s=f.nextInt();
+    HashSet<Integer> h1=new HashSet<Integer>();
+    HashSet<Integer> h2=new HashSet<Integer>();
+    for (int i=0;i<n;i++){
+        int t=f.nextInt();
+        if(t==1){
+            h1.add(i);
+        }
+    }
+    for(int i=0;i<n;i++){
+        int t=f.nextInt();
+        if(t==1){
+            h2.add(i);
+        }
+    }
+    if(h1.contains(0)&&h1.contains(s-1)){
+        System.out.println("YES");
+        System.exit(0);
+    }
+    else if(h1.contains(0)&&h2.contains(s-1)){
+        for (int i=s;i<n;i++){
+            if(h1.contains(i)&&h2.contains(i)){
+                System.out.println("YES");
+                System.exit(0);
+            }
+        }
+    }
+    System.out.println("NO");
+}
+}
