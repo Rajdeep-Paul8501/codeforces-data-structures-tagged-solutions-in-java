d7585e1a0f399b7e6b13ee7fbdcc0c5249a568a8
Rajdeep-Paul8501 <68452736+Rajdeep-Paul8501@users.noreply.github.com>
2020-07-29 18:33:18 +0530

    Create 1371C.java

---
+++ 1371C.java
@@ -0,0 +1,161 @@
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+import java.util.*; 
+import java.util.StringTokenizer;
+import java.io.DataInputStream; 
+import java.io.FileInputStream;
+import java.io.File;
+import java.lang.*;
+import java.net.*;
+
+
+
+  
+public class Main
+{ 
+    static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    }
+    static class urlReader{
+        URL url;
+        BufferedReader br; 
+        StringTokenizer st; 
+
+        public urlReader(URL url1){
+            url = url1;
+            try{
+            br = new BufferedReader(new InputStreamReader(url.openStream()));
+        }
+        catch (IOException  e){
+            System.out.println("GIVEN URL INVALID: " + url);
+        }
+
+        }
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+
+
+    }
+    
+
+  public static void main(String[] args)throws Exception{
+    FastReader f=new FastReader();
+    int t=f.nextInt();
+    while(t-->0){
+        long v=f.nextLong();
+        long c=f.nextLong();
+        long n=f.nextLong();
+        long m=f.nextLong();
+        if (n+m>v+c){
+            System.out.println("NO");
+            continue;
+        }
+        if(Math.min(v,c)<m){
+            System.out.println("NO");
+            continue;
+        }
+        System.out.println("YES");
+    }
+}
+}
+
+
+
+
+    
