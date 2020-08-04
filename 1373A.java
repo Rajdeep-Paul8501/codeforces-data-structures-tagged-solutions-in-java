96df7181ffe1bba9000686651a4520db0ad22296
Rajdeep-Paul8501 <68452736+Rajdeep-Paul8501@users.noreply.github.com>
2020-08-04 20:38:37 +0530

    Create 1373A.java

---
+++ 1373A.java
@@ -0,0 +1,90 @@
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+import java.util.*; 
+import java.util.StringTokenizer;
+import java.io.DataInputStream; 
+import java.io.FileInputStream;
+import java.io.File;
+import java.lang.*;
+import java.net.*;
+
+
+
+  
+public class Main
+{ 
+    static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int cint() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long clong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double cdouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String cline() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    }
+
+ 
+    
+    
+  public static void main(String[] args)throws Exception{
+    FastReader f=new FastReader();
+    int t=f.cint();
+    while(t-->0){
+        long a=f.clong();
+        long b=f.clong();
+        long c=f.clong();
+        if(a>=c)System.out.print(-1+" ");
+        else System.out.print(1+" ");
+        if(a<=(double)c/b)System.out.print(-1+"\n");
+        else System.out.print(b+"\n");
+    }
+}
+}
